0c7d45b0c6bec4a772b25dd856f5851167e5b7b3
Harsh <harshvardhan23.007@gmail.com>
2023-07-12 23:54:55 +0530

    hero banner

--- src/pages/home/heroBanner/heroBanner.jsx
+++ src/pages/home/heroBanner/heroBanner.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import "./style.scss"
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import "./style.scss";
 
 const HeroBanner = () => {
-  return (
-    <div>Hero Banner</div>
-  )
-}
+    const [background, setBackground] = useState("");
+    const [query, setQuery] = useState("");
+    const navigate = useNavigate();
+    const { url } = useSelector((state) => state.home);
 
-export default HeroBanner
+
+    const searchQueryHandler = (event) => {
+        if (event.key === "Enter" && query.length > 0) {
+            navigate(`/search/${query}`);
+        }
+    };
+
+    return (
+        <div className="heroBanner">
+
+            <div className="opacity-layer"></div>
+                <div className="heroBannerContent">
+                    <span className="title">Welcome.</span>
+                    <span className="subTitle">
+                        Millions of movies, TV shows and people to discover.
+                        Explore now.
+                    </span>
+                    <div className="searchInput">
+                        <input
+                            type="text"
+                            placeholder="Search for a movie or tv show...."
+                            onChange={(e) => setQuery(e.target.value)}
+                            onKeyUp={searchQueryHandler}
+                        />
+                        <button>Search</button>
+                    </div>
+                </div>
+        </div>
+    );
+};
+
+export default HeroBanner;
